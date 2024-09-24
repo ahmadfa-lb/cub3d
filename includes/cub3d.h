9cc45c6f12edf5418b8942c857e4cd36606215c5
ahmadfa-lb <farachiahmad2001@hotmail.com>
2024-09-25 00:12:29 +0300

    adding minilibx

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -6,7 +6,25 @@
 /*   By: ahmad <ahmad@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/24 23:59:11 by ahmad             #+#    #+#             */
-/*   Updated: 2024/09/24 23:59:12 by ahmad            ###   ########.fr       */
+/*   Updated: 2024/09/25 00:07:19 by ahmad            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include "../minilibx-linux/mlx.h"
+# include "libft"
+# include <stdio.h>
+# include <stdlib.h>
+# include <stdbool.h>
+# include <fcntl.h>
+# include <math.h>
+# include <sys/time.h>
+
+
+
+
+
+
+#endif
