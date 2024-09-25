6973b5239a2463c45403c84195863597cabbe970
ahmadfa-lb <farachiahmad2001@hotmail.com>
2024-09-25 16:36:49 +0300

    start extracting&storing the settings from the map.cub

--- srcs/parsing/extract_settings.c
+++ srcs/parsing/extract_settings.c
@@ -6,7 +6,7 @@
 /*   By: afarachi <afarachi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 15:33:43 by afarachi          #+#    #+#             */
-/*   Updated: 2024/09/25 15:49:09 by afarachi         ###   ########.fr       */
+/*   Updated: 2024/09/25 16:19:13 by afarachi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,4 +30,18 @@ static void	handle_line(struct s_cub3d_data *data, char *line, int fd, int line_
 	if (!elements)
 		handle_error_and_exit(elements, fd, data);
 	line_length = double_array_len(elements);
+	last_element = elements[line_length - 1];
+	elements[line_length - 1] = ft_strtrim(elements[line_length - 1], "\n");
+	if (!elements[line_length - 1])
+	{
+		free(last_element);
+		handle_error_and_exit(elements, fd, data);
+	}
+	free(last_element);
+	if (data->utils.settings_already_set != BASE_SETTINGS_REQUIRED)
+	{
+		if(line_length != EXPECTED_SETTING_PARTS)
+			handle_error_and_exit(elements, fd, data);
+		store_setting(data, elements, fd);
+	}
 }
---
+++ srcs/parsing/save_essential_settings.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   save_essential_settings.c                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: afarachi <afarachi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/25 16:32:28 by afarachi          #+#    #+#             */
+/*   Updated: 2024/09/25 16:35:13 by afarachi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/cub3d.h"
+
+void	save_essential_setting(struct s_cub3d_data *data, char **line_elements, int fd)
+{
+	if (store_texture_path(data, line_elements, fd) == 0)
+	
+	else if ()
+
+	else
+	{
+		
+	}
+}
