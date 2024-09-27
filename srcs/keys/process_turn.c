d9d5938feb89fbd1199163bc7dbd6c7434611c5e
ahmadfa-lb <farachiahmad2001@hotmail.com>
2024-09-27 16:39:12 +0300

    player turning left

---
+++ srcs/keys/process_turn.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   process_turn.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: afarachi <afarachi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/27 16:34:27 by afarachi          #+#    #+#             */
+/*   Updated: 2024/09/27 16:34:46 by afarachi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/cub3d.h"
+
+void	turn_left(t_cub3d_data *data)
+{
+	float	radian_step;
+
+	radian_step = SENSITIVITY * M_PI / 180;
+	data->player_data.angle -= radian_step;
+	if (data->player_data.angle < 0)
+		data->player_data.angle += 2 * M_PI;
+}
+
