11571cf51c68ebc115de954a0f17c8e27216dca5
ahmadfa-lb <farachiahmad2001@hotmail.com>
2024-09-28 19:57:03 +0300

    adding the main function cub3d.c

--- srcs/cub3d.c
+++ srcs/cub3d.c
@@ -6,8 +6,20 @@
 /*   By: afarachi <afarachi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 00:00:22 by afarachi          #+#    #+#             */
-/*   Updated: 2024/09/28 19:00:56 by afarachi         ###   ########.fr       */
+/*   Updated: 2024/09/28 19:56:29 by afarachi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3d.h"
+
+int	main(int argc, char **argv)
+{
+	t_cub3d_data	cub_data;
+
+	cub_data.utils.argc = argc;
+	cub_data.utils.argv = argv;
+	parsing(&cub_data);
+	load_mlx(&cub_data);
+	game_loop(&cub_data);
+	return (0);
+}
