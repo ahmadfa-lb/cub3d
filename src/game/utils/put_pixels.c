bb4d6103595aabdae2989cbee83d6b3b9d0f1fda
ahmadfa-lb <farachiahmad2001@hotmail.com>
2024-09-30 19:25:37 +0300

    changing files positions

--- src/game/utils/put_pixels.c
+++ src/game/utils/put_pixels.c
@@ -6,11 +6,11 @@
 /*   By: afarachi <afarachi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/27 15:20:35 by afarachi          #+#    #+#             */
-/*   Updated: 2024/09/28 18:59:39 by afarachi         ###   ########.fr       */
+/*   Updated: 2024/09/30 19:22:02 by afarachi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../includes/cub3d.h"
+#include "../../../include/cub3D.h"
 
 int	get_sx_sy(int var1, int var2)
 {
@@ -19,22 +19,7 @@ int	get_sx_sy(int var1, int var2)
 	return (-1);
 }
 
-void	ft_mlx_pixel_put(t_cub3d_data *data, int x, int y, int color)
-{
-	char	*dst;
-
-	if (!data)
-		return ;
-	dst = data->mlx.addr + \
-		(y * data->mlx.line_len + x * (data->mlx.bits / 8));
-	if (dst < data->mlx.addr || dst >= data->mlx.addr + \
-		data->mlx.line_len * data->mlx.win_height)
-		return ;
-	*(unsigned int *)dst = color;
-}
-
-
-void	bresenham_line_draw(t_cub3d_data *data, int x0_y0[2], int x1_y1[2])
+void	bresenham_line_draw(t_cub_data *data, int x0_y0[2], int x1_y1[2])
 {
 	t_bresenham	var;
 
@@ -60,4 +45,18 @@ void	bresenham_line_draw(t_cub3d_data *data, int x0_y0[2], int x1_y1[2])
 			x0_y0[1] += var.sx_sy[1];
 		}
 	}
+}
+
+void	ft_mlx_pixel_put(t_cub_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	if (!data)
+		return ;
+	dst = data->mlx.addr + \
+		(y * data->mlx.line_len + x * (data->mlx.bits / 8));
+	if (dst < data->mlx.addr || dst >= data->mlx.addr + \
+		data->mlx.line_len * data->mlx.win_height)
+		return ;
+	*(unsigned int *)dst = color;
 }
---
+++ src/game/utils/utils.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: afarachi <afarachi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/30 19:22:50 by afarachi          #+#    #+#             */
+/*   Updated: 2024/09/30 19:22:52 by afarachi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../../include/cub3D.h"
+
+int	scale_player_pos(float pos)
+{
+	pos *= MINIMAP_SIZE / (RANGE * 2);
+	return (pos);
+}
+
+void	minimap_frame(t_cub_data *data)
+{
+	t_img	*frame;
+	int		x;
+	int		y;
+	int		color;
+
+	frame = &data->utils.minimap.frame;
+	y = 0;
+	while (y < frame->height)
+	{
+		x = 0;
+		while (x < frame->width)
+		{
+			color = *(int *)(frame->data + y * \
+				frame->size_line + x * (frame->bpp / 8));
+			if ((color & 0xFFFFFF) != BLACK)
+			{
+				*(int *)(data->mlx.addr + y * \
+					data->mlx.line_len + x * (data->mlx.bits / 8)) = color;
+			}
+			x++;
+		}
+		y++;
+	}
+}
+
+long	get_current_time_in_ms(void)
+{
+	struct timeval	tv;
+
+	gettimeofday(&tv, NULL);
+	return ((tv.tv_sec * 1000) + (tv.tv_usec / 1000));
+}
+
+float	deg_to_rad(int a)
+{
+	return (a * M_PI / 180.0);
+}
+
+float	ft_fabs(float value)
+{
+	if (value < 0)
+		return (-value);
+	return (value);
+}
